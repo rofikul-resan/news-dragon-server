264466bc91308144e28283997bc3879e14c0bbc5
rofikul-resan <resan6231@gmail.com>
2023-04-25 15:36:10 +0600

    server setup done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const category = require("./data/categories.json");
+const news = require("./data/news.json");
 const app = express();
 const port = 5000;
 const cors = require("cors");
@@ -12,6 +13,21 @@ app.get("/category", (req, res) => {
   res.send(category);
 });
 
+app.get("/news-category/:id", (req, res) => {
+  const id = req.params.id;
+  if (+id === 0) {
+    res.send(news);
+  } else {
+    const reqNews = news.filter((n) => +n.category_id === +id);
+    res.send(reqNews);
+  }
+});
+
+app.get("/new/:id", (req, res) => {
+  const id = req.params.id;
+  const reqNew = news.find((n) => n._id === id);
+  res.send(reqNew);
+});
 app.listen(port, () => {
   console.log("hay buddy");
 });
